b7d8ba94b63651692e5313da12c6db2d28211bd7
tsubakimoto <tkmbmbr@gmail.com>
2017-07-16 22:38:24 +0900

    [modify] Connection string

--- donetcore_db/DotnetCoreApp.MySql/Program.cs
+++ donetcore_db/DotnetCoreApp.MySql/Program.cs
@@ -4,6 +4,8 @@ namespace DotnetCoreApp.MySql
 {
     class Program
     {
+        private static readonly string connectionString = "Database=fukuten; Data Source=fukuten0728.mysql.database.azure.com; User Id=yuta@fukuten0728; Password={your_password}";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
