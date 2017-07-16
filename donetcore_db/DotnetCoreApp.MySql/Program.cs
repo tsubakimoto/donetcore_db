3d1415024cd5e286378be5be950232ce27d17c30
tsubakimoto <tkmbmbr@gmail.com>
2017-07-16 22:00:08 +0900

    [add] Create mysql project

---
+++ donetcore_db/DotnetCoreApp.MySql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetCoreApp.MySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
