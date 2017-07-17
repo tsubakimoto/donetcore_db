c4c3cd9b7f8e29934a584441766dd8cf474e4d96
tsubakimoto <tkmbmbr@gmail.com>
2017-07-17 21:38:42 +0900

    [modify] Execute sql

--- donetcore_db/DotnetCoreApp.PostgreSql/Program.cs
+++ donetcore_db/DotnetCoreApp.PostgreSql/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Npgsql;
+using static System.Console;
 
 namespace DotnetCoreApp.PostgreSql
 {
     class Program
     {
+        private static readonly string connectionString = "Server=fukuten0728pg.postgres.database.azure.com;Database=fukuten;Port=5432;User Id=yuta@fukuten0728pg;Password={your_password};";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <remarks>
+        /// create table users (id serial not null primary key, name varchar(128) not null);
+        /// insert into users(name) values('yuta'), ('tsubaki');
+        /// </remarks>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM users", connection))
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string row = $"{reader["id"]}\t{reader["name"]}";
+                        WriteLine(row);
+                    }
+                }
+
+                connection.Clone();
+            }
+
+            ReadLine();
         }
     }
 }
